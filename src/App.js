fb7a0740a7e8a8abb36b48210b496f35dadb983f
Baptiste LECHAT <baptistelechat@outlook.fr>
2021-04-11 14:29:39 +0200

    🎉 initialize project and add basics npm  modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import readXlsxFile from 'read-excel-file'
+
 
 function App() {
+
+  const readExcel = (file) => {
+    readXlsxFile(file, { getSheets: true }).then((sheets) => {
+      // Get the list of sheets
+      // sheets === [{ name: 'Sheet1' }, { name: 'Sheet2' }]
+      console.log(sheets)
+      readXlsxFile(file, {sheet: 3}).then((rows) => {
+        // `rows` is an array of rows
+        // each row being an array of cells.
+        console.log(rows)
+      })
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +35,13 @@ function App() {
         >
           Learn React
         </a>
+        <input
+          type="file"
+          onChange={(e) => {
+            const file = e.target.files[0];
+            readExcel(file);
+          }}
+        />
       </header>
     </div>
   );
